edacb436e5a3565b8bafa9bd74ca6d2edcd6476b
Desiwroclaw <harshilkumawat28@gmail.com>
2025-05-23 02:08:53 +0200

    Create menu.js

---
+++ src/menu.js
@@ -0,0 +1,29 @@
+const menu = {
+  "Naan Rolls": [
+    {
+      id: 1,
+      name: "Chicken Roll",
+      description: "Chicken Seekh / Pickled Onion / Tikka Sauce / Lettuce / Mint Chutney",
+      price: 37,
+      image: "/menu/naan-roll.jpg"
+    },
+    {
+      id: 2,
+      name: "Paneer Roll",
+      description: "Paneer Tikka / Salad / Chutneys / Fresh Naan",
+      price: 37,
+      image: "/menu/naan-roll.jpg"
+    }
+  ],
+  "Dessert & Drinks": [
+    {
+      id: 99,
+      name: "Mango Lassi",
+      description: "",
+      price: 15.5,
+      image: "/menu/mango-lassi.jpg"
+    }
+  ]
+};
+
+export default menu;
