200ac5d6e44262a1b3cc9bb76e9c11fa14600073
Desiwroclaw <harshilkumawat28@gmail.com>
2025-05-23 16:19:33 +0200

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -2,22 +2,152 @@ const menu = {
   "Naan Rolls": [
     {
       id: 1,
-      name: "Chicken Roll",
-      description: "Chicken Seekh / Pickled Onion / Tikka Sauce / Lettuce / Mint Chutney",
-      price: 37,
+      name: "Lamb Roll",
+      description: "Lamb Seekh / Pickled Onion / Tikka Sauce / Fresh Coriander & Mint / Lettuce / Green Raita",
+      price: 39,
       image: "/menu/naan-roll.jpg"
     },
     {
       id: 2,
-      name: "Paneer Roll",
-      description: "Paneer Tikka / Salad / Chutneys / Fresh Naan",
+      name: "Chicken Roll",
+      description: "Chicken Seekh / Pickled Onion / Tikka Sauce / Fresh Coriander & Mint / Lettuce / Green Raita",
       price: 37,
+      image: "/menu/chicken-roll.png"
+    },
+    {
+      id: 3,
+      name: "Paneer Roll",
+      description: "Paneer cheese / pickled onion / tikka sauce / fresh coriander and mint / lettuce / green raita",
+      price: 37,
+      image: "/menu/naan-roll.jpg"
+    },
+    {
+      id: 4,
+      name: "Veggie Roll",
+      description: "Falafel / pickled onion / tikka sauce/ fresh coriander and mint / lettuce / green raita",
+      price: 33,
       image: "/menu/naan-roll.jpg"
     }
   ],
+  "Desi Bowls": [
+    {
+      id: 5,
+      name: "Butter Chicken",
+      description: "Delicate and creamy butter tomato sauce with cashew nuts and spices",
+      price: 43,
+      image: "/menu/butter-chicken.png"
+    },
+    {
+      id: 6,
+      name: "Butter Paneer",
+      description: "Delicate and creamy butter tomato sauce with cashew nuts and spices",
+      price: 43,
+      image: "/menu/butter-paneer.png"
+    },
+    {
+      id: 7,
+      name: "Butter Shrimps",
+      description: "Delicate and creamy butter tomato sauce with cashew nuts and spices",
+      price: 50,
+      image: "/menu/shrimp-bbq.png"
+    },
+    {
+      id: 8,
+      name: "Chicken Tikka Masala",
+      description: "Slightly spicy tomato and onion sauce",
+      price: 43,
+      image: "/menu/tikka-masala.png"
+    },
+    {
+      id: 9,
+      name: "Paneer Tikka Masala",
+      description: "Slightly spicy tomato and onion sauce",
+      price: 43,
+      image: "/menu/tikka-masala.png"
+    },
+    {
+      id: 10,
+      name: "Lamb Tikka Masala",
+      description: "Slightly spicy tomato and onion sauce",
+      price: 50,
+      image: "/menu/tikka-masala.png"
+    },
+    {
+      id: 11,
+      name: "Chicken Palak Curry",
+      description: "Slightly spicy sauce with fresh spinach",
+      price: 43,
+      image: "/menu/tikka-masala.png"
+    },
+    {
+      id: 12,
+      name: "Paneer Palak Curry",
+      description: "Slightly spicy sauce with fresh spinach",
+      price: 43,
+      image: "/menu/tikka-masala.png"
+    },
+    {
+      id: 13,
+      name: "Lamb Palak Curry",
+      description: "Slightly spicy sauce with fresh spinach",
+      price: 50,
+      image: "/menu/tikka-masala.png"
+    },
+    {
+      id: 14,
+      name: "Lamb Curry",
+      description: "Slow cooked lamb with Rajasthani spices",
+      price: 50,
+      image: "/menu/tikka-masala.png"
+    }
+  ],
+  "Tandoori BBQ Platters": [
+    {
+      id: 15,
+      name: "TANDOORI CHICKEN",
+      description: "chicken tikka 3 styled marination / baked potatoes / mango coleslaw / raita (320g)",
+      price: 47,
+      image: "/menu/tandoori-chicken.png"
+    },
+    {
+      id: 16,
+      name: "SHEEKH PLATTER",
+      description: "chicken tikka 3 styled marination / baked potatoes / mango coleslaw / raita (320g)",
+      price: 54,
+      image: "/menu/seekh-kebab.png"
+    },
+    {
+      id: 17,
+      name: "SMOKEY GARLIC SHRIMPS",
+      description: "king prawns / baked potatoes / mango coleslaw / raita (8 pcs.)",
+      price: 55,
+      image: "/menu/shrimp-bbq.png"
+    }
+  ],
+  "Smallish": [
+    {
+      id: 18,
+      name: "DUMPLINGS MOMO (6 pcs.)",
+      description: "Tibetan dumplings with chicken. Served with tomato-sesame sauce",
+      price: 26,
+      image: "/menu/momos.jpg"
+    },
+    {
+      id: 19,
+      name: "SAMOSA (2 pcs.)",
+      description: "",
+      price: 19,
+      image: "/menu/momos.jpg"
+    }
+  ],
+  "Breads": [
+    { id: 20, name: "Naan", description: "", price: 10, image: "/menu/naan-roll.jpg" },
+    { id: 21, name: "Butter Naan", description: "", price: 11, image: "/menu/naan-roll.jpg" },
+    { id: 22, name: "Garlic Butter Naan", description: "", price: 13, image: "/menu/naan-roll.jpg" }
+  ],
   "Dessert & Drinks": [
     {
-      id: 99,
+      id: 23,
       name: "Mango Lassi",
       description: "",
       price: 15.5,
